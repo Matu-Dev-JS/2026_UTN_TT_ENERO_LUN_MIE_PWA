e70e1e1f7542c65113d5c47ca35136b1a4751abb
matu.dev <mati.a.gimenez2002@gmail.com>
2026-01-12 17:53:23 -0300

    Clase 1

--- Clase 1/script.js
+++ Clase 1/script.js
@@ -40,16 +40,16 @@ falsy:
 */
 
 /* 
-- findIndex (6)
-- Metodos avanzados de array (3)
-- spread operator (...) (3)
-- for of  (2)
-- for in  (1)
-- For (1)
+- findIndex VISTO
+- Metodos avanzados de array VISTO
+- spread operator (...) VISTO
+- for of  VISTO
+- for in  VISTO
+- For VISTO
 - callbacks (1)
 */
 
-const personas = [
+/* const personas = [
     {
         id: 1, 
         nombre: 'Sergio'
@@ -62,7 +62,7 @@ const personas = [
         id: 3, 
         nombre: 'Javier'
     },
-]
+] */
 
 //Eliminar a el usuario con id 2
 
@@ -95,3 +95,217 @@ personas.findIndex(
     (persona) => Number(persona.id) === Number(2)
 
 ) */
+
+/* 
+Filter:
+Nos permite obtener a partir de un array otro array donde cada elemento cumpla con una x condicion 
+NO MUTABLE (no modifica el objeto original)
+Recorre todo el array
+SIEMPRE DEVUELVE UN ARRAY 
+*/
+/* const galletitas = [
+    {
+        id: 8,
+        tipo: 'rumba'
+    },
+    {   
+        id: 1,
+        tipo: 'alfajorcito'
+    },
+    {   
+        id: 2,
+        tipo: 'sonrisas'
+    },
+    {   
+        id: 5,
+        tipo: 'sonrisas'
+    },
+    {
+        id: 3,
+        tipo: "melba"
+    },
+    {
+        id: 7,
+        tipo: "melba"
+    },
+    {
+        id: 4,
+        tipo: 'rumba'
+    }
+] */
+
+//Quiero obtener todas las rumbas de la bolsa de galletitas
+/* const rumbas = galletitas.filter(
+    (galletita) => {
+        return galletita.tipo === 'rumba'
+    }
+)
+
+if(rumbas.length){
+    console.log('Hay rumbas')
+} */
+
+//Quiero obtener una rumba
+//Find lo usamos para obtener cierto elemento del array, una particularidad es que find devuelve la referencia del objeto dentro del array
+
+/* const rumba = galletitas.find(
+    (
+        (galletita) => galletita.tipo === 'rumba'
+    )
+)
+
+
+rumba.caracteristica = 'partida a la mitad'
+
+console.log(galletitas)
+ */
+
+/* SPREAD OPERATOR */
+/* 
+Lo vamos a utilizar para romper referencias (clonacion)
+*/
+/* const persona_1 = {
+    name: 'pepe',
+    edad: 40
+}
+
+let nombre_propiedad_nueva = 'estatura'
+let valor_propiedad_nueva = '176cm'
+const persona_2 = {...persona_1, ['edad']: 50, color: 'red', [nombre_propiedad_nueva]: valor_propiedad_nueva}
+
+persona_1.name = 'juan'
+
+console.log(persona_2) */
+
+/* const nombres = ['pepe', 'juan']
+
+const nombres_2 = [...nombres, 'maria']
+console.log(nombres_2) */
+
+/* 
+FOR:
+Repetir la ejecucion de un bloque de codigo
+Lo usamos cuando podemos conocer o saber la cantidad de veces que vamos a repetir algo
+For tiene un contador que le indicara si debe repetir o no la ejecucion del bloque de codigo
+
+tenemos una lista de ganancias trimestrales
+
+*/
+
+/* const ganancias_trimestrales = [900, 400, 300, 1500] */
+
+//Averiguar el promedio de las ganancias trimestrales
+// sumatoria de notas / cantidad de notas
+
+//El contador ira del 0 al 3
+/* let sumatoria_ganancias = 0
+for(
+    let contador = 0; 
+    contador < ganancias_trimestrales.length; 
+    contador = contador + 1
+){
+    const ganancia = ganancias_trimestrales[contador]
+    sumatoria_ganancias = sumatoria_ganancias + ganancia
+}
+
+let resultado = sumatoria_ganancias / ganancias_trimestrales.length
+console.log("el promedio de ganancia trimestral es: $" + resultado )
+ */
+
+//FOR OF
+/* 
+Sirve para recorrer elementos iterables
+Se lo suele usar principalmente para recorrer ARRAYS
+ */
+/* let sumatoria_ganancias = 0 
+
+for(const ganancia_trimestral of ganancias_trimestrales){
+    sumatoria_ganancias = sumatoria_ganancias + ganancia_trimestral
+}
+
+let resultado = sumatoria_ganancias / ganancias_trimestrales.length
+console.log("el promedio de ganancia trimestral es: $" + resultado )
+ */
+
+//FOR IN
+//Nos permite recorrer un objeto, recorremos las propiedades de un objeto
+
+/* const persona = {
+    "D.N.I": 11222333,
+    "Nombre": 'Pepe',
+    "Apellidos": "Suarez",
+    "Pais nacimento": "argentina"
+}
+
+let mensaje = 'Los datos del usuario son:\n'
+
+for(let propiedad in persona){
+    const valor = persona[propiedad]
+    mensaje = mensaje + `${propiedad}: ${valor}\n`
+}
+
+console.log(mensaje) */
+
+//Callbacks
+/* 
+Las callbacks nos permiten transmitir funciones a otras funciones
+Es como si te escribiera en hoja "salta" y te la diera, pero te digo que ejecutes lo que dice la hoja luego de alejarte 10 metros
+*/
+
+/* function alejarse10MetrosYHacerAlgo(algoCallback){
+    console.log('Me alejo 10 metros...')
+    algoCallback()
+}
+
+alejarse10MetrosYHacerAlgo(
+    () => {
+        console.log("saltar")
+    }
+) */
+
+/* function filtro (array, filtroCallback){
+    const resultado = []
+    for(const elemento of array){
+        if(filtroCallback(elemento)){
+            resultado.push(elemento)
+        }
+    }
+    return resultado
+}
+
+const nombres = ['pepe', 'juan', 'pepe']
+
+console.log(filtro(nombres, (nombre) => nombre === 'pepe')) */
+
+
+/* function solicitarDatoNumerico (mensaje){
+    let dato = prompt(mensaje)
+    while(!dato || isNaN(dato)){
+        alert('Debes ingresar un numero')
+        dato = prompt(mensaje)
+    }
+    return Number(dato)
+} */
+
+/* function solicitarDato (mensaje, error, condicionCallback){
+    let dato = prompt(mensaje)
+    while(!condicionCallback(dato)){
+        alert(error)
+        dato = prompt(mensaje)
+    }
+    return dato
+}
+
+let nombre = solicitarDato(
+    'Ingrese el nombre', 
+    'Error debes ingresar un texto mayor a 3 caracteres', 
+    (dato) => dato && dato.length > 3
+) */
+
+const btn = document.getElementById("btn-confirmar-compra")
+btn.addEventListener(
+    'click',
+    () => {
+        console.log('compra confirmada')
+    }
+)
