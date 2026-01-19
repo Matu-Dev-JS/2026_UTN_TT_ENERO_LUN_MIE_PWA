b8971a11fd80e2398323c6eb7a2cf87ac604adb5
matu.dev <mati.a.gimenez2002@gmail.com>
2026-01-19 16:32:07 -0300

    Clase-3

--- clase-3/src/main.ts
+++ clase-3/src/main.ts
@@ -1,19 +1,19 @@
 
-let message : string | number = 'hello world';
+/* let message : string | number = 'hello world';
 let nombre : null | string = null
 let estaActivo : boolean = false
-
+ */
 //Una forma de hacer tipado de objetos
-let persona : {
+/* let persona : {
     nombre: string,
     edad: number
 } = {
     nombre: 'pepe',
     edad: 40
-}
+} */
 
 //Es mejor esta forma
-interface Persona {
+/* interface Persona {
     nombre: string,
     edad?: number,
     dni?: string
@@ -30,4 +30,30 @@ let persona_3 : Persona = {
     dni: '11222333'
 }
 
-console.log(message)
+const notas : string[] = ['pepe']
+const personas : Persona[] = [persona_2, persona]
+
+function sumar (a: number, b: number): number{
+    return a + b
+}
+
+function saludar (nombre: string): void{
+    alert('Hola ' + nombre)
+}
+
+let dato_random : any = undefined
+ */
+
+/* Carrito de compras */
+/* 
+Guardar en una variable el estado del carrito de compras
+Cada elemento del carrito sera { id: number, quantity: number, title: string }
+Crear las siguientes funciones:
+    - agregarAlCarrito(id, title, cantidad)
+    - eliminarDelCarrito(id) : Busca el item por id y lo elimina
+    - quitarUnaUnidadDelCarrito(id) : Busca el item por id y verifica la cantidad
+        - si es 1 entonces lo elimina
+        - si es mas de uno entonces decrementa su cantidad en 1
+    - vaciarCarrito() : Vacia el carrito
+    - calcularTotal() : Retornara un numero con el total del carrito
+*/
