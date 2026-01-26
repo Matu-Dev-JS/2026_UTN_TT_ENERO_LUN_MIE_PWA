8633a2b60ab2b9fc4e2d3840de24cc26d56235f5
matu.dev <mati.a.gimenez2002@gmail.com>
2026-01-26 15:28:54 -0300

    Clase 5

---
+++ Clase-5-TS-POO-Practica/src/main.ts
@@ -0,0 +1,33 @@
+
+/* 
+type Role = 'user' | 'admin' | 'support'
+type Number = number
+
+let user_role : Role = 'hola' 
+
+*/
+/* type Direction = 'TOP' | "DOWN" | 'RIGHT' | "LEFT"
+let current_direction : Direction = 'arriba' */
+/* type Person = string | {nombre: string}
+let persona : Person = 90 */
+
+/* 
+Puestos posibles: 'Developer' | 'Designer' | 'Marketing' | 'Proyect Manager' | 'HR' 
+
+Crear las siguientes clases:
+
+Empleado
+- nombre
+- sueldo
+- fecha_contratacion
+- id_empleado
+- puesto: Puesto
+- presentarse(): Debe decir por consola: "Hola soy {nombre} y trabajo como {puesto}" 
+
+Pasante (es un empleado)
+- tiempo_de_contrato_en_meses
+- hacerCosasDePasante(): 
+  - Si es un dev debe decir por consola "tire mi cafe por error"
+  - Sino solo decir "deberia terminar a las 10 pero saldre a las 12"
+
+*/
