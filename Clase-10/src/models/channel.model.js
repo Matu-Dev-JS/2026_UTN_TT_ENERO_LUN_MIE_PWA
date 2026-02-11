a4634e7534c02a7314ae1e865a218fa9d2f04011
matu.dev <mati.a.gimenez2002@gmail.com>
2026-02-11 17:40:44 -0300

    Modelos de canal y mensajes

--- Clase-10/src/models/channel.model.js
+++ Clase-10/src/models/channel.model.js
@@ -6,3 +6,29 @@ Channel
 -created_at
 
 */
+
+import mongoose from "mongoose";
+const channelSchema = new mongoose.Schema(
+    {
+        fk_id_workspace: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Workspace',
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: false,
+        },
+        created_at: {
+            type: Date,
+            required: true,
+            default: Date.now,
+        },
+    }
+)
+const Channel = mongoose.model("Channel", channelSchema)
+export default Channel
--- Clase-10/src/models/channelMessages.model.js
+++ Clase-10/src/models/channelMessages.model.js
@@ -6,3 +6,31 @@ ChannelMessages
 -created_at
 
 */
+
+import mongoose from "mongoose";
+
+const channelMessagesSchema = new mongoose.Schema({
+    fk_id_channel: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Channel",
+        required: true
+    },
+    fk_id_member: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "WorkspaceMember",
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    created_at: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
+
+const ChannelMessages = mongoose.model("ChannelMessage", channelMessagesSchema)
+
+export default ChannelMessages
