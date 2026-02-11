5c6b72dc6ce4bd4878aa87b0bc311b3955b36c2e
matu.dev <mati.a.gimenez2002@gmail.com>
2026-02-11 16:43:56 -0300

    clase 10

--- Clase-10/src/main.js
+++ Clase-10/src/main.js
@@ -2,6 +2,7 @@ import ENVIRONMENT from "./config/environment.config.js"
 import connectMongoDB from "./config/mongoDB.config.js"
 import User from "./models/user.model.js"
 import Workspace from "./models/workspace.model.js"
+import WorkspaceMember from "./models/workspaceMember.model.js"
 
 
 
@@ -28,3 +29,13 @@ createWorkspace(
 );
 
 createWorkspace("Salida"); */
+
+async function createMember (id_user, id_workpace, role = 'user'){
+    await WorkspaceMember.create({
+        fk_id_user: id_user,
+        fk_id_workspace: id_workpace,
+        role: role
+    })
+}
+
+//createMember('698ccf5288e8976b62166133', '698cd99696d860463f81e5fc', 'owner')
