26f6ee76021cfe3d885edb5f89a513dfcfca2e6d
matu.dev <mati.a.gimenez2002@gmail.com>
2026-02-11 15:31:20 -0300

    Clase 10

--- Clase-10/src/main.js
+++ Clase-10/src/main.js
@@ -1,4 +1,7 @@
 import ENVIRONMENT from "./config/environment.config.js"
+import connectMongoDB from "./config/mongoDB.config.js"
 
 console.log('hola mundo')
 console.log(ENVIRONMENT.MONGO_DB_CONNECTION_STRING)
+
+connectMongoDB()
