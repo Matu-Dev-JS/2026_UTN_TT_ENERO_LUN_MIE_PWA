61b84dfa073fcef180eae6f4ba6e0f9265a9b524
matu.dev <mati.a.gimenez2002@gmail.com>
2026-02-16 15:54:25 -0300

    Clase 11

---
+++ Clase-11/src/repository/member.repository.js
@@ -0,0 +1,7 @@
+/* 
+WorkspaceMemberRepository
+    - create (fk_id_user, fk_id_workspace, role)
+    - updateRole(id_member, role)
+    - delete(id_member)
+    - getMemberList(workspace_id) //Obtiene lista de miembros relacionados a ese espacio de trabajo
+*/
