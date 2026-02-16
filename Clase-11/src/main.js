a6c281a35f504fe5da877235989ab4e5ba22c0e9
matu.dev <mati.a.gimenez2002@gmail.com>
2026-02-16 17:11:51 -0300

    clase 11

--- Clase-11/src/main.js
+++ Clase-11/src/main.js
@@ -1,9 +1,10 @@
 import ENVIRONMENT from "./config/environment.config.js"
 import connectMongoDB from "./config/mongoDB.config.js"
-import User from "./models/user.model.js"
-import Workspace from "./models/workspace.model.js"
+/* import User from "./models/user.model.js"
+import Workspace from "./models/workspace.model.js" */
 import WorkspaceMember from "./models/workspaceMember.model.js"
 import workspaceMemberRepository from "./repository/member.repository.js"
+import userRepository from "./repository/user.repository.js"
 import workspaceRepository from "./repository/workspace.repository.js"
 
 
@@ -15,3 +16,5 @@ connectMongoDB()
 //workspaceMemberRepository.create('69936f9cbc903a2cb76c14e9','698ccf5288e8976b62166133', 'owner')
 
 workspaceMemberRepository.getMemberList('69936f9cbc903a2cb76c14e9')
+
+
