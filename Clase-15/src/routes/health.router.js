cdec174e26701d17f2fabe4255e553e9d70d532f
matu.dev <mati.a.gimenez2002@gmail.com>
2026-03-04 16:39:28 -0300

    Health Controller

--- Clase-15/src/routes/health.router.js
+++ Clase-15/src/routes/health.router.js
@@ -1,45 +1,17 @@
 import express from 'express'
-import userRepository from '../repository/user.repository.js'
+
+import healthController from '../controllers/health.controller.js'
 
 const healthRouter = express.Router()
 
 healthRouter.get(
     '/',
-    (request, response) => {
-        response.status(200).json(
-            {
-                message: "La API funciona correctamente",
-                status: 200,
-                ok: true
-            }
-        )
-    }
+    healthController.getApi
 )
 
 healthRouter.get(
     '/database',
-    async (request, response) => {
-        try{
-            await userRepository.getUser()
-            return response.status(200).json(
-                {
-                    message: "La DB funciona correctamente",
-                    status: 200,
-                    ok: true
-                }
-            )
-        }
-        catch(error){
-            console.error("ERROR EN LA DB:", error)
-            return response.status(500).json(
-                {
-                    message: 'La DB esta fallando, contactarse con el administrador',
-                    status: 500,
-                    ok: false
-                }
-            )
-        }
-    }
+    healthController.getDB
 )
 
 export default healthRouter
