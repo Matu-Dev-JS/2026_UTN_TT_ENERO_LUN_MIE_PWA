d8e754adf2eade64aa42d6197c22abe5d3be8342
matu.dev <mati.a.gimenez2002@gmail.com>
2026-03-16 16:15:42 -0300

    Login y registro

--- frontend/src/Screens/LoginScreen/LoginScreen.jsx
+++ frontend/src/Screens/LoginScreen/LoginScreen.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const LoginScreen = () => {
+    const LOGIN_FORM_FIELDS = {
+        EMAIL: 'email',
+        PASSWORD: 'password'
+    }
+
   return (
-    <div>LoginScreen</div>
+    <div>
+        <h1>
+            Iniciar sesion
+        </h1>
+        <form>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email"  name={LOGIN_FORM_FIELDS.EMAIL} />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name={LOGIN_FORM_FIELDS.PASSWORD} />
+            </div>
+            <button type="submit">Iniciar sesion</button>
+        </form>
+        <span>No tienes una cuenta? <Link to="/register">Registrarse</Link></span>
+    </div>
   )
 }
 
--- frontend/src/Screens/RegisterScreen/RegisterScreen.jsx
+++ frontend/src/Screens/RegisterScreen/RegisterScreen.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 const RegisterScreen = () => {
+     const REGISTER_FORM_FIELDS = {
+        EMAIL: 'email',
+        PASSWORD: 'password'
+    }
+
   return (
-    <div>RegisterScreen</div>
+    <div>
+        <h1>
+            Registrarse
+        </h1>
+        <form>
+            <div>
+                <label htmlFor="name">Name</label>
+                <input type="text" id="name" name={REGISTER_FORM_FIELDS.NAME} />
+            </div>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email"  name={REGISTER_FORM_FIELDS.EMAIL} />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="password" id="password" name={REGISTER_FORM_FIELDS.PASSWORD} />
+            </div>
+            <button type="submit">Registrarse</button>
+        </form>
+        <span>Ya tienes una cuenta? <Link to="/login">Iniciar sesion</Link></span>
+    </div>
   )
 }
 
